4da8ca6b56863179f31adf102d8892159a19dbe7
vansin <msnode@163.com>
2022-11-27 17:21:55 +0800

    Feat: update

--- configs/_base_/datasets/icdar2019_qbox.py
+++ configs/_base_/datasets/icdar2019_qbox.py
@@ -68,7 +68,7 @@ val_dataloader = dict(
 test_dataloader = val_dataloader
 
 val_evaluator = dict(
-    type='DOTAMetric', metric='mAP', iou_thrs=0.9, predict_box_type='qbox')
+    type='DOTAMetric', metric='mAP', iou_thrs=[0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95], predict_box_type='qbox')
 test_evaluator = val_evaluator
 
 # inference on test dataset and format the output results
