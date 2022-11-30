4120524e8bffcbdec8de7101dc4038f69803e6be
vansin <msnode@163.com>
2022-11-30 23:55:22 +0800

    feat: update

--- configs/roi_trans/roi-trans-le90_r50_fpn_12x_dota.py
+++ configs/roi_trans/roi-trans-le90_r50_fpn_12x_dota.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/datasets/icdar2019_rbox.py', '../_base_/schedules/schedule_1x.py',
+    '../_base_/datasets/icdar2019_rbox.py', '../_base_/schedules/schedule_12x.py',
     '../_base_/default_runtime_rbox.py'
 ]
 
