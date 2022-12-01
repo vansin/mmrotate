f2a4953c9ee38fe720675e8e8ab1c616205580b1
vansin <msnode@163.com>
2022-12-01 18:45:47 +0800

    feat: update

--- configs/gliding_vertex/gliding-vertex-rbox_r50_fpn_1x_dota.py
+++ configs/gliding_vertex/gliding-vertex-rbox_r50_fpn_1x_dota.py
@@ -1,6 +1,6 @@
 _base_ = [
-    '../_base_/datasets/dota_qbox.py', '../_base_/schedules/schedule_1x.py',
-    '../_base_/default_runtime.py'
+    '../_base_/datasets/dota_rbox.py', '../_base_/schedules/schedule_1x.py',
+    '../_base_/default_runtime_rbox.py'
 ]
 
 model = dict(
