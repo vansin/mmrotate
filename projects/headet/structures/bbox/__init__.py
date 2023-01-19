7c16508e0cf7ae46f7362c20e51983ca589b7e86
vansin <msnode@163.com>
2023-01-20 00:30:47 +0800

    update rbox2qbox

--- projects/headet/structures/bbox/__init__.py
+++ projects/headet/structures/bbox/__init__.py
@@ -1,7 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 
-from .rotated_boxes import RotatedBoxes
+# from .rotated_boxes import RotatedBoxes, rbox2qbox
+from .rotated_boxes import RotatedBoxes, rbox2qbox
 
 __all__ = [
-    'RotatedBoxes'
+    'RotatedBoxes', 'rbox2qbox'
 ]
