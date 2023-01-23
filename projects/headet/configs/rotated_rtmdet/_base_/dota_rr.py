141a7060146ab9fce88729ae5ecb6b0050c23728
vansin <msnode@163.com>
2023-01-23 15:42:34 +0800

    update the test_evaluator

--- projects/headet/configs/rotated_rtmdet/_base_/dota_rr.py
+++ projects/headet/configs/rotated_rtmdet/_base_/dota_rr.py
@@ -90,9 +90,28 @@ val_dataloader = dict(
         pipeline=val_pipeline))
 test_dataloader = val_dataloader
 
-val_evaluator = dict(type='DOTAMetric', metric='mAP', iou_thrs=[
-                     0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95])
-test_evaluator = val_evaluator
+val_evaluator = [
+    dict(
+        type='DOTAHeadMetric',
+        metric='mAP',
+        iou_thrs=[0.5],
+        hangle_thr=90),
+    dict(
+        type='DOTAMetric',
+        metric='mAP',
+        iou_thrs=[0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95])
+]
+test_evaluator = [
+    dict(
+        type='DOTAHeadMetric',
+        metric='mAP',
+        iou_thrs=[0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95],
+        hangle_thr=90),
+    dict(
+        type='DOTAMetric',
+        metric='mAP',
+        iou_thrs=[0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95])
+]
 
 # inference on test dataset and format the output results
 # for submission. Note: the test set has no annotation.
