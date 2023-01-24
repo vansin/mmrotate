b0c8620dc8f93239b1526ce71f1372f1dea516c7
vansin <msnode@163.com>
2023-01-24 17:39:50 +0800

    update metric

--- projects/headet/configs-ic19-rbb-rbb/_base_/datasets/dota.py
+++ projects/headet/configs-ic19-rbb-rbb/_base_/datasets/dota.py
@@ -73,22 +73,22 @@ test_dataloader = val_dataloader
 
 
 val_evaluator = [
-    dict(
-        type='DOTAHeadMetric',
-        metric='mAP',
-        iou_thrs=[0.5],
-        hangle_thr=90),
+    # dict(
+    #     type='DOTAHeadMetric',
+    #     metric='mAP',
+    #     iou_thrs=[0.5],
+    #     hangle_thr=90),
     dict(
         type='DOTAMetric',
         metric='mAP',
         iou_thrs=[0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95])
 ]
 test_evaluator = [
-    dict(
-        type='DOTAHeadMetric',
-        metric='mAP',
-        iou_thrs=[0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95],
-        hangle_thr=90),
+    # dict(
+    #     type='DOTAHeadMetric',
+    #     metric='mAP',
+    #     iou_thrs=[0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95],
+    #     hangle_thr=90),
     dict(
         type='DOTAMetric',
         metric='mAP',
